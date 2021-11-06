5de3275211b4e89acd304b68cac384e863e1c4d4
chaudeveloper <ms_michanguyen@yahoo.com>
2021-11-05 23:14:36 -0400

    create all window promts, confirm and alerts

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Assignment code here
 
+window.prompt("How many characters would you like your password to have? (at least 8 characters and no more than 128 characters)")
+
+window.confirm("Would you like to include lowercase?")
+
+window.confirm("Would you like to include uppercase?")
+
+window.confirm("Would you like to include numeric?")
+
+window.prompt("Which special characters would you like to use in your password? These are the characters you can use (!#$%&'()*+,-./:;<=>?@[\]^_`{|}~)")
+
+window.confirm("Are you sure that you'd like to stop generating your unique password?")
+
+window.alert("You need to provide a valid answer! Please try again.")
+
+window.alert ("Goodbye! Refresh the page to generate your unique password again.")
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
